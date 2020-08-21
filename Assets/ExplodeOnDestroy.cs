c340600d853c4c6f0509293e8e1315944bec1176
Cash Garman <cashgarman@hotmail.co.uk>
2020-08-21 11:41:35 -0700

    Lesson 10

---
+++ Assets/ExplodeOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnDestroy : MonoBehaviour
+{
+    public GameObject explosionEffectPrefab;
+
+    private void OnDestroy()
+    {
+        Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/VRHand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHand : MonoBehaviour
+{
+    public string triggerName;
+    private Color originalColor;
+
+    void Start()
+    {
+        originalColor = GetComponent<MeshRenderer>().material.color;
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis(triggerName) > 0.2)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = originalColor;
+        }
+    }
+}
